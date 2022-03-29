2ea485e5f3a3b589410f9a50a62ce9c5832b40f7
jlmoer <jlmoerdijk@gmail.com>
2022-03-29 17:32:39 +0200

    Exercise Finished

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 // Model:
-const User = require("./userModel");
+const Student = require("./models/studentModel");
+const Address = require("./models/addressModel");
+
 
 // Middleware:
 app.use(express.json());
@@ -20,6 +22,29 @@ mongoose
         }
     )
     .then(() => console.log("Connected to MongoDB"));
+
+// Routes:
+
+// First Route:
+app.post("/students", async (req, res) => {
+
+    const address = await Address.create(req.body.address);
+    const student = new Student({
+        firstName: req.body.id,
+        surName: req.body.student.surName,
+        address: address.id,
+    });
+    await student.save();
+
+    res.status(201).send("Student succesfully created");
+});
+
+// Second route:
+app.get("/students/:id", async (req, res) => {
+    const student = await Student.findById(req.params.id).populate("address");
+
+    res.json(student);
+});
 
 // Server start:
 app.listen(8000, () => {
